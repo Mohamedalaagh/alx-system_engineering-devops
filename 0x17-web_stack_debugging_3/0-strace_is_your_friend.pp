e412ab17db012a353c8059b975a7ebd1999cfc31
Your Name <you@example.com>
2024-08-14 20:18:05 +0300

    moala

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,40 +1,6 @@
-# Install Apache
-package { 'apache2':
-  ensure => installed,
-}
+# Puppet manifest to fix a bug in wp-setings.php
 
-# Install PHP
-package { 'php':
-  ensure => installed,
-}
-
-# Install WordPress
-package { 'wordpress':
-  ensure => installed,
-  require => Package['apache2'], # Ensure Apache is installed before WordPress
-}
-
-# Configure Apache to serve WordPress
-file { '/etc/apache2/sites-available/wordpress.conf':
-  ensure  => file,
-  content => "
-<VirtualHost *:80>
-    DocumentRoot /usr/share/wordpress
-    <Directory /usr/share/wordpress>
-        AllowOverride All
-    </Directory>
-</VirtualHost>
-  ",
-  require => Package['apache2'], # Ensure Apache is installed before configuration
-}
-
-# Enable the WordPress site and reload Apache
-exec { 'enable_wordpress_site':
-  command => '/usr/sbin/a2ensite wordpress',
-  require => File['/etc/apache2/sites-available/wordpress.conf'],
-}
-
-exec { 'reload_apache':
-  command => '/usr/sbin/service apache2 reload',
-  require => Exec['enable_wordpress_site'],
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
